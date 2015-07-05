198dafefeb1163e32eb79512c337aba829c72e78
Greg Bowering <gerg.bowering@gmail.com>
2015-07-05 15:09:12 +0930

    Iteration 2 debugging.  ome progress with network loading of History SA places via JSON but still some crashes.

--- android/AR_POI/src/com/hypearth/arpoi/CustomWorldHelper.java
+++ android/AR_POI/src/com/hypearth/arpoi/CustomWorldHelper.java
@@ -15,9 +15,15 @@
  */
 package com.hypearth.arpoi;
 
+import android.app.ProgressDialog;
 import android.content.Context;
 import android.util.Log;
 
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.VolleyLog;
+import com.android.volley.toolbox.JsonObjectRequest;
 import com.beyondar.android.world.GeoObject;
 import com.beyondar.android.world.World;
 
@@ -27,6 +33,9 @@ import org.json.JSONObject;
 
 public class CustomWorldHelper {
     public static final int LIST_TYPE_EXAMPLE_1 = 1;
+
+    // Tag used to cancel the JSON HTTP request
+    static final String tag_json_obj = "json_obj_req";
 
     public static World sharedWorld;
 
@@ -44,33 +53,62 @@ public class CustomWorldHelper {
 
         // sharedWorld.setGeoPosition(41.90533734214473d, 2.565848038959814d);
 
-        try {
             Log.i(CustomWorldHelper.class.getName(), "generateObjects() attempting to get History SA places");
-            JSONObject historySAPlaces = JsonFetcher.readJsonFromUrl("http://data.history.sa.gov.au/sahistoryhub/place");
-            Log.i(CustomWorldHelper.class.getName(), "Got History SA Places");
-            JSONArray features = historySAPlaces.getJSONArray("features");
-            if (features != null) {
-                Log.i(CustomWorldHelper.class.getName(), "Got History SA Features, count=" + features.length());
-                for (int i = 0; i < features.length(); i++) {
-                    JSONObject feature = features.getJSONObject(i);
-                    JSONObject geometry = feature.getJSONObject("geometry");
-                    JSONArray coordinates = feature.getJSONArray("coordinates");
-                    double lng = coordinates.getDouble(0);
-                    double lat = coordinates.getDouble(1);
-                    JSONObject properties = feature.getJSONObject("properties");
-                    String title = properties.getString("TITLE");
-                    GeoObject go1 = new GeoObject(2000l + i);
-                    go1.setGeoPosition(lat, lng);
-                    go1.setImageUri("https://data.sa.gov.au/data/uploads/group/20150513-041856.087869historysalogo.png");
-                    go1.setName(title);
-                    // Add the GeoObject to the world
-                    sharedWorld.addBeyondarObject(go1);
-                    // sharedWorld.addBeyondarObject(go1, LIST_TYPE_EXAMPLE_1); // TODO color-code Places/Events/Organisations/photos etc
+        String url = "http://data.history.sa.gov.au/sahistoryhub/place";
+
+        final ProgressDialog pDialog = new ProgressDialog(context);
+        pDialog.setMessage("Loading History SA places...");
+        pDialog.show();
+
+        JsonObjectRequest jsonObjReq = new JsonObjectRequest(Request.Method.GET,
+                url, null,
+                new Response.Listener<JSONObject>() {
+
+                    @Override
+                    public void onResponse(final JSONObject response) {
+                        Log.d(CustomWorldHelper.class.getName(), response.toString());
+                        pDialog.hide();
+
+                        Log.i(CustomWorldHelper.class.getName(), "Got History SA Places");
+                        try {
+                            JSONArray features = response.getJSONArray("features");
+                            if (features != null) {
+                                Log.i(CustomWorldHelper.class.getName(), "Got History SA Features, count=" + features.length());
+                                for (int i = 0; i < features.length(); i++) {
+                                    JSONObject feature = features.getJSONObject(i);
+                                    JSONObject geometry = feature.getJSONObject("geometry");
+                                    JSONArray coordinates = feature.getJSONArray("coordinates");
+                                    double lng = coordinates.getDouble(0);
+                                    double lat = coordinates.getDouble(1);
+                                    JSONObject properties = feature.getJSONObject("properties");
+                                    String title = properties.getString("TITLE");
+                                    GeoObject go1 = new GeoObject(2000l + i);
+                                    go1.setGeoPosition(lat, lng);
+                                    go1.setImageUri("https://data.sa.gov.au/data/uploads/group/20150513-041856.087869historysalogo.png");
+                                    go1.setName(title);
+                                    // Add the GeoObject to the world
+                                    sharedWorld.addBeyondarObject(go1);
+                                    // sharedWorld.addBeyondarObject(go1, LIST_TYPE_EXAMPLE_1); // TODO color-code Places/Events/Organisations/photos etc
+                                }
+                            }
+
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }, new Response.ErrorListener() {
+
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                VolleyLog.d(CustomWorldHelper.class.getName(), "Error: " + error.getMessage());
+                pDialog.hide();
                 }
-            }
-        } catch (JSONException e) {
-            e.printStackTrace();
-        }
+        }) {
+
+        };
+
+        // Adding request to request queue
+        AppController.getInstance().addToRequestQueue(jsonObjReq, tag_json_obj);
 
         return sharedWorld;
     }
--- android/AR_POI/src/com/hypearth/arpoi/JsonFetcher.java
+++ android/AR_POI/src/com/hypearth/arpoi/JsonFetcher.java
@@ -1,60 +1,19 @@
 package com.hypearth.arpoi;
 
-import android.os.StrictMode;
-import android.util.Log;
+import android.content.Context;
 
-import org.apache.http.HttpEntity;
-import org.apache.http.HttpResponse;
-import org.apache.http.StatusLine;
-import org.apache.http.client.ClientProtocolException;
-import org.apache.http.client.HttpClient;
-import org.apache.http.client.methods.HttpGet;
-import org.apache.http.impl.client.DefaultHttpClient;
 import org.json.JSONException;
 import org.json.JSONObject;
-
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
 
 /**
  * Created by Greg Bowering on 5/07/2015.
  */
 public abstract class JsonFetcher {
 
-    public static JSONObject readJsonFromUrl(final String url) throws JSONException {
-        StringBuilder builder = new StringBuilder();
-        HttpClient client = new DefaultHttpClient();
-        HttpGet httpGet = new HttpGet(url);
-        try {
-            // Just for testing, allow network access in the main thread
-            // NEVER use this is productive code
-            StrictMode.ThreadPolicy originalPolicy = StrictMode.getThreadPolicy();
-            StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
-            StrictMode.setThreadPolicy(policy);
+    public static final String TAG = JsonFetcher.class.getSimpleName();
 
-            HttpResponse response = client.execute(httpGet);
-            StatusLine statusLine = response.getStatusLine();
-            int statusCode = statusLine.getStatusCode();
-            StrictMode.setThreadPolicy(originalPolicy);
+    public static JSONObject readJsonFromUrl(final Context context, final String url) throws JSONException {
 
-            if (statusCode == 200) {
-                HttpEntity entity = response.getEntity();
-                InputStream content = entity.getContent();
-                BufferedReader reader = new BufferedReader(new InputStreamReader(content));
-                String line;
-                while ((line = reader.readLine()) != null) {
-                    builder.append(line);
-                }
-            } else {
-                Log.e(JsonFetcher.class.toString(), "Failed to download file");
-            }
-        } catch (ClientProtocolException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        return new JSONObject(builder.toString());
+        return new JSONObject("");
     }
 }
